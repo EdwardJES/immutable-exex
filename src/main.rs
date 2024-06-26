c2a09344e61361727e3c4d75af91c030df88f897
James Snewin <james.snewin@immutable.com>
2024-06-27 08:13:39 +1000

    add deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+use eyre::Ok;
+use reth_primitives::{address, Address};
+
+const IMMUTABLE_BRIDGE: Address = address!("Ba5E35E26Ae59c7aea6F029B68c6460De2d13eB6");
+
+fn main() -> eyre::Result<()>{
     println!("Hello, world!");
+
+    Ok(())
 }
