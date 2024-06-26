760b6cf7e5db64a5b2d44c004afdaf1b044eaa54
James Snewin <james.snewin@immutable.com>
2024-06-27 08:30:01 +1000

    add abi and events

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use eyre::Ok;
+use alloy_sol_types::{sol};
 use reth_primitives::{address, Address};
+
+sol!(RootERC20Bridge, "root_erc20_bridge_abi.json");
+use RootERC20Bridge::{ChildChainERC20Deposit, IMXDeposit, WETHDeposit, NativeEthDeposit, RootChainERC20Withdraw, RootChainETHWithdraw};
 
 const IMMUTABLE_BRIDGE: Address = address!("Ba5E35E26Ae59c7aea6F029B68c6460De2d13eB6");
 
-fn main() -> eyre::Result<()>{
+fn main() -> eyre::Result<()> {
     println!("Hello, world!");
 
     Ok(())
